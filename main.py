262caf4bad26ea015b40f4602e4b47527b2303e2
adityadenny <aditaydenny6@gmail.com>
2022-03-09 15:00:58 +0700

    learn tutorial selenium

---
+++ main.py
@@ -0,0 +1,9 @@
+# Selenium Tutorial #1
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://techwithtim.net")
