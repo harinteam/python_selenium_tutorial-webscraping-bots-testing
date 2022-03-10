3cca8b62c236a1cacdd1983ec2204f682a83bdbc
adityadenny <aditaydenny6@gmail.com>
2022-03-10 08:28:22 +0700

    learn selenium tutorial tech with tim

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # Selenium Tutorial #1
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = "/usr/local/bin/chromedriver"
+driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("python")
+search.send_keys(Keys.RETURN)
+
+print(driver.page_source)
+
+time.sleep(5)
+
+driver.quit()
